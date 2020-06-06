307875a41f5261ed8cb7f9d4628b2cdeac5b4eaa
mihailm94 <mihailmilchev@gmail.com>
2020-06-06 14:11:35 +0200

    WIP: Add buttons to qdialog. WIP

--- pintor_color.cpp
+++ pintor_color.cpp
@@ -2,5 +2,20 @@
 
 pintorColor::pintorColor()
 {
+    /* Do not disable access to windows underneath this one */
+    this->setModal(false);
+
+    buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel);
+
+        connect(buttonBox, &QDialogButtonBox::accepted, this, &QDialog::accept);
+        connect(buttonBox, &QDialogButtonBox::rejected, this, &QDialog::reject);
+
+    QWidget colorMainWidget;
+    colorMainWidget.setParent(this);
+
+    QComboBox colorModels;
+    colorModels.addItem("RGB");
+    colorModels.addItem("YUV");
+
 
 }
--- pintor_color.h
+++ pintor_color.h
@@ -2,11 +2,16 @@
 #define PINTORCOLOR_H
 
 #include <QDialog>
+#include <QComboBox>
+#include <QDialogButtonBox>
 
 class pintorColor : public QDialog
 {
 public:
     pintorColor();
+
+protected:
+    QDialogButtonBox buttonBox;
 };
 
 #endif // PINTORCOLOR_H
