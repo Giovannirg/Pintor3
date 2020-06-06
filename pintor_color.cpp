6508d2d400501f8bded3aa654329d077e0a819f0
mihailm94 <mihailmilchev@gmail.com>
2020-06-06 16:34:43 +0200

    Add basic buttons and layout

--- pintor_color.cpp
+++ pintor_color.cpp
@@ -1,10 +1,41 @@
+#include <QComboBox>
+#include <QPushButton>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+
 #include "pintor_color.h"
 
-pintorColor::pintorColor()
+pintorColor::pintorColor(QWidget *parent)
+    : QDialog(parent) /* Pass on the parent parameter to the base class constructor. */
 {
     /* Do not disable access to windows underneath this one */
     this->setModal(false);
 
+    colorModels = new QComboBox(this);
+    colorModels->addItem("RGB");
+    colorModels->addItem("YUV");
+
+    /* Ampersands indicate shortcuts */
+    buttonPreview = new QPushButton(tr("&Preview"), this);
+    buttonClose = new QPushButton(tr("&Close"), this);
+    buttonApply = new QPushButton(tr("&Apply"), this);
+
+    QHBoxLayout *colorModelsLayout = new QHBoxLayout;
+    colorModelsLayout->addStretch();
+    colorModelsLayout->addWidget(colorModels);
+    colorModelsLayout->addStretch();
+
+    QHBoxLayout *buttonLayout = new QHBoxLayout;
+    buttonLayout->addWidget(buttonPreview);
+    buttonLayout->addStretch();
+    buttonLayout->addWidget(buttonClose);
+    buttonLayout->addWidget(buttonApply);
+
+    QVBoxLayout *mainLayout = new QVBoxLayout;
+    mainLayout->addLayout(colorModelsLayout);
+    mainLayout->addLayout(buttonLayout);
+    setLayout(mainLayout);
+    /*
     buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel);
 
         connect(buttonBox, &QDialogButtonBox::accepted, this, &QDialog::accept);
@@ -16,6 +47,6 @@ pintorColor::pintorColor()
     QComboBox colorModels;
     colorModels.addItem("RGB");
     colorModels.addItem("YUV");
-
+    */
 
 }
--- pintor_color.h
+++ pintor_color.h
@@ -2,16 +2,41 @@
 #define PINTORCOLOR_H
 
 #include <QDialog>
-#include <QComboBox>
-#include <QDialogButtonBox>
+
+/* Forward declaration of classes used
+ * Tells the compiler that a class
+ * exists, without providing the
+ * class definition */
+class QPushButton;
+class QComboBox;
 
 class pintorColor : public QDialog
 {
-public:
-    pintorColor();
+    /* Necessary for all classes that define signals or slots. */
+    Q_OBJECT
 
-protected:
-    QDialogButtonBox buttonBox;
+signals:
+    /* Which signals are emitted when a user presses a button for example */
+
+/* Expands into a construct that the C++ compiler can digest. (Same as signals) */
+private slots:
+
+
+public:
+    pintorColor(QWidget *parent = NULL);
+
+private:
+    /* Forward declaration of classes used.
+     * Pointers, so that we don't access them in the header
+     * file, so the compiler doesn't need the full class definitions.
+     */
+    QPushButton *buttonClose;
+    QPushButton *buttonApply;
+    QPushButton *buttonPreview;
+
+    QComboBox *colorModels;
+
+
 };
 
 #endif // PINTORCOLOR_H
